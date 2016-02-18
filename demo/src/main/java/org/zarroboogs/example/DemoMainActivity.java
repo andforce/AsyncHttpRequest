9b8ed1675e39aa31bd1d80d049527744b04843af
wangdiyuan <wangdiyuan@smartisan.com>
2016-02-18 20:28:45 +0800

    add progress request api

--- demo/src/main/java/org/zarroboogs/example/DemoMainActivity.java
+++ demo/src/main/java/org/zarroboogs/example/DemoMainActivity.java
@@ -2,12 +2,14 @@ package org.zarroboogs.example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import org.zarroboogs.http.AsyncHttpPostString;
 import org.zarroboogs.http.AsyncHttpRequest;
 import org.zarroboogs.http.AsyncHttpResponse;
 import org.zarroboogs.http.AsyncHttpResponseHandler;
+import org.zarroboogs.http.AsyncHttpResponseProgressHandler;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -23,6 +25,29 @@ public class DemoMainActivity extends AppCompatActivity {
 
         mTextView = (TextView) findViewById(R.id.show_http_response);
         final AsyncHttpRequest request = new AsyncHttpRequest();
+
+        request.get("http://www.aqtxt.com/", new AsyncHttpResponseProgressHandler() {
+            @Override
+            public void onUpdate(long bytesRead, long contentLength) {
+                mTextView.setText("" + bytesRead + " / " +contentLength);
+                Log.d("MyUpdateLoad", "update");
+            }
+
+            @Override
+            public void onFailure(IOException e) {
+                Log.d("MyUpdateLoad", "onFailure");
+
+            }
+
+            @Override
+            public void onSuccess(AsyncHttpResponse response) {
+                Log.d("MyUpdateLoad", "onSuccess");
+
+            }
+        });
+
+
+
 //        request.get("http://www.baidu.com", new AsyncHttpResponseHandler() {
 //            @Override
 //            public void onFailure(IOException e) {
@@ -36,18 +61,18 @@ public class DemoMainActivity extends AppCompatActivity {
 //            }
 //        });
 
-
-        request.post("https://api.github.com/markdown/raw", new AsyncHttpPostString("text/x-markdown; charset=utf-8", "test"), new AsyncHttpResponseHandler() {
-            @Override
-            public void onFailure(IOException e) {
-                mTextView.setText(e.toString());
-            }
-
-            @Override
-            public void onSuccess(AsyncHttpResponse response) {
-                mTextView.setText(response.getBody());
-            }
-        });
+//
+//        request.post("https://api.github.com/markdown/raw", new AsyncHttpPostString("text/x-markdown; charset=utf-8", "test"), new AsyncHttpResponseHandler() {
+//            @Override
+//            public void onFailure(IOException e) {
+//                mTextView.setText(e.toString());
+//            }
+//
+//            @Override
+//            public void onSuccess(AsyncHttpResponse response) {
+//                mTextView.setText(response.getBody());
+//            }
+//        });
 
     }
 }
