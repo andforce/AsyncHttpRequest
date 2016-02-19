682d57f3d2e476f459864a55b92b860aecd66596
wangdiyuan <wangdiyuan@smartisan.com>
2016-02-19 13:54:59 +0800

    add cookie manager

--- async-http-request/src/main/java/org/zarroboogs/http/AsyncHttpRequest.java
+++ async-http-request/src/main/java/org/zarroboogs/http/AsyncHttpRequest.java
@@ -1,13 +1,21 @@
 package org.zarroboogs.http;
 
 import java.io.IOException;
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.Cookie;
+import okhttp3.CookieJar;
 import okhttp3.FormBody;
 import okhttp3.Headers;
+import okhttp3.HttpUrl;
 import okhttp3.Interceptor;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -15,18 +23,26 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
+import okhttp3.internal.Internal;
 import okio.Buffer;
 import okio.BufferedSource;
 import okio.ForwardingSource;
 import okio.Okio;
 import okio.Source;
+import static java.util.logging.Level.WARNING;
+import static okhttp3.internal.Util.delimiterOffset;
+import static okhttp3.internal.Util.trimSubstring;
 
 /**
  * Created by wangdiyuan on 16-2-17.
  */
 public class AsyncHttpRequest {
-    private final OkHttpClient mOkHttpClient = new OkHttpClient();
+    private  OkHttpClient mOkHttpClient;
 
+    public AsyncHttpRequest(){
+        CookieManager cookieManager = new CookieManager();
+        mOkHttpClient = new OkHttpClient.Builder().cookieJar(new JavaNetCookieJar(cookieManager)).build();
+    }
 
     public void post(String url, AsyncHttpHeaders headers, Map<String, String> formData, final AsyncHttpResponseHandler responseHandler) {
         Request.Builder requestBuilder = new Request.Builder();
@@ -314,6 +330,88 @@ public class AsyncHttpRequest {
             if (responseProgressHandler != null) {
                 responseProgressHandler.sendUpdateMessage(bytesRead, contentLength);
             }
+        }
+    }
+
+    public final class JavaNetCookieJar implements CookieJar {
+        private final CookieHandler cookieHandler;
+
+        public JavaNetCookieJar(CookieHandler cookieHandler) {
+            this.cookieHandler = cookieHandler;
+        }
+
+        @Override public void saveFromResponse(HttpUrl url, List<Cookie> cookies) {
+            if (cookieHandler != null) {
+                List<String> cookieStrings = new ArrayList<>();
+                for (Cookie cookie : cookies) {
+                    cookieStrings.add(cookie.toString());
+                }
+                Map<String, List<String>> multimap = Collections.singletonMap("Set-Cookie", cookieStrings);
+                try {
+                    cookieHandler.put(url.uri(), multimap);
+                } catch (IOException e) {
+                    Internal.logger.log(WARNING, "Saving cookies failed for " + url.resolve("/..."), e);
+                }
+            }
+        }
+
+        @Override public List<Cookie> loadForRequest(HttpUrl url) {
+            // The RI passes all headers. We don't have 'em, so we don't pass 'em!
+            Map<String, List<String>> headers = Collections.emptyMap();
+            Map<String, List<String>> cookieHeaders;
+            try {
+                cookieHeaders = cookieHandler.get(url.uri(), headers);
+            } catch (IOException e) {
+                Internal.logger.log(WARNING, "Loading cookies failed for " + url.resolve("/..."), e);
+                return Collections.emptyList();
+            }
+
+            List<Cookie> cookies = null;
+            for (Map.Entry<String, List<String>> entry : cookieHeaders.entrySet()) {
+                String key = entry.getKey();
+                if (("Cookie".equalsIgnoreCase(key) || "Cookie2".equalsIgnoreCase(key))
+                        && !entry.getValue().isEmpty()) {
+                    for (String header : entry.getValue()) {
+                        if (cookies == null) cookies = new ArrayList<>();
+                        cookies.addAll(decodeHeaderAsJavaNetCookies(url, header));
+                    }
+                }
+            }
+
+            return cookies != null
+                    ? Collections.unmodifiableList(cookies)
+                    : Collections.<Cookie>emptyList();
+        }
+
+        /**
+         * Convert a request header to OkHttp's cookies via {@link HttpCookie}. That extra step handles
+         * multiple cookies in a single request header, which {@link Cookie#parse} doesn't support.
+         */
+        private List<Cookie> decodeHeaderAsJavaNetCookies(HttpUrl url, String header) {
+            List<Cookie> result = new ArrayList<>();
+            for (int pos = 0, limit = header.length(), pairEnd; pos < limit; pos = pairEnd + 1) {
+                pairEnd = delimiterOffset(header, pos, limit, ";,");
+                int equalsSign = delimiterOffset(header, pos, pairEnd, '=');
+                String name = trimSubstring(header, pos, equalsSign);
+                if (name.startsWith("$")) continue;
+
+                // We have either name=value or just a name.
+                String value = equalsSign < pairEnd
+                        ? trimSubstring(header, equalsSign + 1, pairEnd)
+                        : "";
+
+                // If the value is "quoted", drop the quotes.
+                if (value.startsWith("\"") && value.endsWith("\"")) {
+                    value = value.substring(1, value.length() - 1);
+                }
+
+                result.add(new Cookie.Builder()
+                        .name(name)
+                        .value(value)
+                        .domain(url.host())
+                        .build());
+            }
+            return result;
         }
     }
 }
